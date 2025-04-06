595aad4935b2b03c08ae4ade63b3bbb7d2de0d0e
Volodymyr-Stupnyk <vstupnik44@gmail.com>
2025-04-06 22:46:22 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,56 @@
-import FriendList from "./components/FriendList/FriendList";
-import Profile from "./components/Profile/Profile";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import userData from "./assets/userData.json";
-import transactions from './assets/transactions.json';
-import friends from './assets/friends.json';
+import React, { useState, useEffect } from 'react'; 
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from './components/Notification/Notification';
 
 
-const App = () => {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends}/>
-      <TransactionHistory items={transactions}/>
-    </>);
-};
 
-export default App;
+const App = () => { 
+  const initialFeedback = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+  const [feedback, setFeedback] = useState(() => {  
+      const savedFeedback = localStorage.getItem('feedback');  
+      return savedFeedback ? JSON.parse(savedFeedback) : initialFeedback;  
+  });  
 
-  [
-  {
-    "avatar": "https://cdn-icons-png.flaticon.com/512/1998/1998592.png",
-    "name": "Mango",
-    "isOnline": true,
-    "id": 1812
-  },
-  {
-    "avatar": "https://cdn-icons-png.flaticon.com/512/616/616438.png",
-    "name": "Kiwi",
-    "isOnline": false,
-    "id": 1137
-  },
-  {
-    "avatar": "https://cdn-icons-png.flaticon.com/512/1623/1623681.png",
-    "name": "Ajax",
-    "isOnline": true,
-    "id": 1213
-  },
-  {
-    "avatar": "https://cdn-icons-png.flaticon.com/512/2977/2977285.png",
-    "name": "Jay",
-    "isOnline": true,
-    "id": 1714
-  },
-  {
-    "avatar": "https://cdn-icons-png.flaticon.com/512/1998/1998749.png",
-    "name": "Poly",
-    "isOnline": false,
-    "id": 1284
-  }
-]
+  useEffect(() => {  
+      localStorage.setItem('feedback', JSON.stringify(feedback));  
+  }, [feedback]);  
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;  
+  const updateFeedback = (feedbackType) => {  
+      setFeedback((prevFeedback) => ({  
+          ...prevFeedback,  
+          [feedbackType]: prevFeedback[feedbackType] + 1  
+      }));  
+  }; 
+  const positiveFeedback = Math.round((feedback.good * 100) / totalFeedback);
+
+  const resetFeedback = () => {  
+      setFeedback(initialFeedback);  
+  };  
+
+  return (  
+    <div>
+      <Description/>
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback} />  
+      {totalFeedback > 0 ? (  
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback} />  
+      ) : (  
+          <Notification/>  
+      )}  
+    </div>  
+  );  
+};  
+
+export default App; 
